2a9393deea5f836662b7c52837bb673b17bb5ce4
Devang Dayal <devangdayal@gmail.com>
2026-05-20 19:47:12 +0530

    Added First Non repeating Character in String

---
+++ problem_solving/FirstUniqueCharacter.java
@@ -0,0 +1,28 @@
+public class FirstUniqueCharacter {
+    public void firstUniqChar(String s) {
+        int[] charCount = new int[26];
+        s = s.toLowerCase(); 
+
+        for(char c : s.toCharArray()){
+            charCount[c-'a']++;
+        }
+
+        for(int i=0;i<s.length();i++){
+            if(charCount[s.charAt(i)-'a']==1){
+                System.out.println(s.charAt(i) + " is the first unique character with index" + i + " in the string.");
+                return;
+            }
+        
+        }
+
+        System.out.println("No unique character found.");
+    }
+
+    public static void main(String[] args) {
+        FirstUniqueCharacter solution = new FirstUniqueCharacter();
+        String input = "GeeksforGeeks";
+        solution.firstUniqChar(input);
+    }
+
+
+}
