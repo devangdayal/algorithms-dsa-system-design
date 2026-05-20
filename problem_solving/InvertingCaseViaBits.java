a5c8c905e6579c86f0060c3c7bc3b4bf0961f936
Devang Dayal <devangdayal@gmail.com>
2026-05-20 18:58:47 +0530

    Adding Case Inversion via Bits Manipulation

---
+++ problem_solving/InvertingCaseViaBits.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class InvertingCaseViaBits {
+    public String toggleCaseViaBits(String s) {
+        char[] arr = s.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (Character.isAlphabetic(arr[i])) {
+                arr[i] ^= (1 << 5);
+            }
+        }
+        return new String(arr);
+    }
+
+    public static void main(String[] args) {
+        InvertingCaseViaBits invert = new InvertingCaseViaBits();
+        String input;
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter a string to toggle case: ");
+            input = scanner.nextLine();
+        }
+        String toggled = invert.toggleCaseViaBits(input);
+        System.out.println("Original: " + input);
+        System.out.println("Toggled via Bits: " + toggled);
+    }
+}
