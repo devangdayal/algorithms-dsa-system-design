65bbb4de7bacfdf5a4f187c96222f09b737a0722
Devang Dayal <devangdayal@gmail.com>
2026-05-26 13:26:47 +0530

    Zig Zag Diagonal Traversal

---
+++ problem_solving/Matrix/ZigZagTraversal.java
@@ -0,0 +1,53 @@
+// 498. Diagonal Traverse
+// Given an m x n matrix mat, return an array of all the elements of the array in a diagonal order.
+
+package Matrix;
+
+public class ZigZagTraversal {
+    public int[] findDiagonalOrder(int[][] mat) {
+
+        if (mat == null || mat.length == 0)
+            return new int[0];
+
+        int m = mat.length, n = mat[0].length;
+        int[] result = new int[m * n];
+        int row = 0, col = 0;
+
+        for (int i = 0; i < (m * n); i++) {
+
+            result[i] = mat[row][col];
+
+            if ((row + col) % 2 == 0) {
+                if (col == n - 1)
+                    row++;
+                else if (row == 0)
+                    col++;
+                else {
+                    row--;
+                    col++;
+                }
+            } else {
+                if (row == m - 1)
+                    col++;
+                else if (col == 0)
+                    row++;
+                else {
+                    row++;
+                    col--;
+                }
+            }
+
+        }
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        ZigZagTraversal zigZagTraversal = new ZigZagTraversal();
+        int[][] mat = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int[] result = zigZagTraversal.findDiagonalOrder(mat);
+        for (int i : result) {
+            System.out.print(i + " ");
+        }
+    }
+}
