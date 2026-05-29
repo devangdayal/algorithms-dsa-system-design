5e5f49a85534a1648b8a8ce803a0cf3fd6a06f58
Devang Dayal <devangdayal@gmail.com>
2026-05-29 16:23:28 +0530

    Added Hashing

--- practice_sheet/learn_the_basics/basic_recursions/RecursionBasics.java
+++ practice_sheet/learn_the_basics/basic_recursions/RecursionBasics.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RecursionBasics {
 
@@ -6,7 +8,7 @@ public class RecursionBasics {
         if (n == 0) {
             return;
         }
-        System.out.print(n+" ");
+        System.out.print(n + " ");
         n--;
         printNTo1(n);
     }
@@ -15,35 +17,35 @@ public class RecursionBasics {
         if (n == 0) {
             return;
         }
-        print1ToN(n-1);
+        print1ToN(n - 1);
         System.out.print(n + " ");
 
     }
 
-    public void sumN(int n,int sum){
-        if(n==0){
+    public void sumN(int n, int sum) {
+        if (n == 0) {
             System.out.println(sum);
             return;
         }
-        sumN(n-1,sum+n);
+        sumN(n - 1, sum + n);
     }
 
-    public void factorialN(int n, int fact){
-        if(n==1){
+    public void factorialN(int n, int fact) {
+        if (n == 1) {
             System.out.println(fact);
             return;
         }
-        factorialN(n-1, fact*n);
+        factorialN(n - 1, fact * n);
     }
 
-    public void reverseArray(int[] numbers,int left, int right){
-        if(left>=right){
+    public void reverseArray(int[] numbers, int left, int right) {
+        if (left >= right) {
             return;
         }
         int temp = numbers[left];
-        numbers[left]=numbers[right];
+        numbers[left] = numbers[right];
         numbers[right] = temp;
-        reverseArray(numbers,left+1,right-1);
+        reverseArray(numbers, left + 1, right - 1);
     }
 
     public void palindromeFlag(String str, int left, int right) {
@@ -69,6 +71,50 @@ public class RecursionBasics {
         }
     }
 
+    public void frequencyArray(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+
+    public void highestFrequencyArray(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+
+        // Count frequencies
+        for (int i = 0; i < n; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        int maxFreq = 0, minFreq = n;
+        int maxEle = 0, minEle = 0;
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int element = entry.getKey();
+            int count = entry.getValue();
+            if (count > maxFreq) {
+                maxFreq = count;
+                maxEle = element;
+            }
+            if (count < minFreq) {
+                minFreq = count;
+                minEle = element;
+            }
+        }
+
+        System.out.println("The highest frequency element is: " + maxEle);
+        System.out.println("The lowest frequency element is: " + minEle);
+    }
+
+
     public static void main(String[] args) {
         int n = 5;
 
@@ -82,31 +128,42 @@ public class RecursionBasics {
         recursionBasics.print1ToN(n);
         System.out.println("\n=========");
 
-        System.out.println("Sum of "+ n +" : ");
-        recursionBasics.sumN(n,0);
+        System.out.println("Sum of " + n + " : ");
+        recursionBasics.sumN(n, 0);
         System.out.println("=========");
 
-        System.out.println("Factorial of "+ n +" : ");
-        recursionBasics.factorialN(n,1);
+        System.out.println("Factorial of " + n + " : ");
+        recursionBasics.factorialN(n, 1);
         System.out.println("=========");
 
 
-        int[] numbers = {1,2,3,4,5,6,7,8,9,10};
-        System.out.println("Array of "+ Arrays.toString(numbers));
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("Array of " + Arrays.toString(numbers));
 //      Arrays.stream(numbers).forEach(System.out::println);
-        recursionBasics.reverseArray(numbers,0,numbers.length-1);
-        System.out.println("Reverse of Array : "+ Arrays.toString(numbers));
+        recursionBasics.reverseArray(numbers, 0, numbers.length - 1);
+        System.out.println("Reverse of Array : " + Arrays.toString(numbers));
         System.out.println("=========");
 
 
         String str = "Racecar";
-        System.out.println("Is "+str+" a palindrome?");
-        recursionBasics.palindromeFlag(str.toLowerCase(),0,str.length()-1);
+        System.out.println("Is " + str + " a palindrome?");
+        recursionBasics.palindromeFlag(str.toLowerCase(), 0, str.length() - 1);
         System.out.println("=========");
 
-        System.out.println("Fibonacci Series for "+n+": ");
+        System.out.println("Fibonacci Series for " + n + ": ");
         recursionBasics.printFibonacciSeries(n);
         System.out.println("\n=========");
+
+
+        int[] numbers2 = {10, 5, 10, 15, 10, 5};
+        System.out.println("Frequency of Element in the array:  " + Arrays.toString(numbers));
+        recursionBasics.frequencyArray(numbers2);
+        System.out.println("=========");
+
+
+        System.out.println("Highest Frequency Element in the array:  " + Arrays.toString(numbers));
+        recursionBasics.highestFrequencyArray(numbers2);
+        System.out.println("=========");
 
     }
 }
