df2f6086ff68e8c6e33ef1ffb58aca07597a0ee4
Devang Dayal <devangdayal@gmail.com>
2026-06-05 18:05:10 +0530

    Added Printing array with Max Sum Array

--- practice_sheet/array_problems/medium/KadaneAlgo_MaxSumArray.java
+++ practice_sheet/array_problems/medium/KadaneAlgo_MaxSumArray.java
@@ -2,19 +2,38 @@
 // Given an integer array nums, find the subarray with the largest sum, and return its sum.
 
 
+import java.util.Arrays;
+
 public class KadaneAlgo_MaxSumArray {
 
     public int maxSumArray(int[] numbers) {
-
         int maxCurrent = numbers[0], maxGlobal = numbers[0];
-
-        for(int i=1;i<numbers.length;i++){
-            maxCurrent = Math.max(numbers[i], maxCurrent+numbers[i]);
-            maxGlobal = Math.max(maxGlobal,maxCurrent);
+        for (int i = 1; i < numbers.length; i++) {
+            maxCurrent = Math.max(numbers[i], maxCurrent + numbers[i]);
+            maxGlobal = Math.max(maxGlobal, maxCurrent);
         }
-
         return maxGlobal;
+    }
 
+    public int[] printMaxSumArray(int[] numbers) {
+        int maxCurrent = numbers[0], maxGlobal = numbers[0];
+        int currentStart = 0, bestStart = 0, bestEnd = 0;
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > maxCurrent + numbers[i]) {
+                maxCurrent = numbers[i];
+                currentStart = i;
+            } else {
+                maxCurrent += numbers[i];
+            }
+
+            if (maxCurrent > maxGlobal) {
+                maxGlobal = maxCurrent;
+                bestStart = currentStart;
+                bestEnd = i;
+            }
+        }
+        return Arrays.copyOfRange(numbers, bestStart,bestEnd+1);
     }
 
     static void main(String[] args) {
@@ -22,5 +41,7 @@ public class KadaneAlgo_MaxSumArray {
         KadaneAlgo_MaxSumArray kadaneAlgoMaxSumArray = new KadaneAlgo_MaxSumArray();
 
         System.out.println("Maximum subarray sum is: " + kadaneAlgoMaxSumArray.maxSumArray(arr));
+        System.out.println("Maximum subarray sum is: " + Arrays.toString(kadaneAlgoMaxSumArray.printMaxSumArray(arr)));
+
     }
 }
