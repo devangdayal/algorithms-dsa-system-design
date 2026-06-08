0851b1cdb031ef90530fd2a633c87c20b9bce6f1
Devang Dayal <devangdayal@gmail.com>
2026-06-08 17:01:48 +0530

    Upper and Lower Bound

---
+++ practice_sheet/binary_search/one_d/LowerUpperBound.java
@@ -0,0 +1,44 @@
+public class LowerUpperBound {
+
+    static void main(String[] args) {
+        int[] numbers = {1, 3, 5, 7, 9};
+        int target = 6;
+        LowerUpperBound lowerBound = new LowerUpperBound();
+        System.out.println(lowerBound.findLowerBound(numbers, target));
+        System.out.println(lowerBound.findUpperBound(numbers, target));
+    }
+
+    public int findLowerBound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int bound = 0;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] >= target) {
+                bound = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+
+        }
+        return bound;
+    }
+
+    public int findUpperBound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int bound = 0;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > target) {
+                bound = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+
+        }
+        return bound;
+    }
+}
