afb5a0f4a06895c7c783c390ddd42d569b11c1a1
Devang Dayal <devangdayal@gmail.com>
2026-06-10 16:33:03 +0530

    Quick Sort

--- practice_sheet/learn_searching_sorting/sorting_II/QuickSort.java
+++ practice_sheet/learn_searching_sorting/sorting_II/QuickSort.java
@@ -2,17 +2,36 @@ import java.util.Arrays;
 
 public class QuickSort {
 
-    public void quickSort(int[] nums){
+    public void quickSort(int[] nums, int low, int high){
         int n = nums.length;
+        if(low<high){
+            int pivot = partition(nums,low,high);
+            quickSort(nums,low,pivot-1);
+            quickSort(nums, pivot+1, high);
+        }
+    }
 
 
+    public int partition(int[] nums, int low, int high){
+        int pivot = nums[high];
+
+        int i = low-1;
+        for(int j = low;j<=high-1;j++){
+            if(nums[j] < pivot){
+                i++;
+                Swap.swap(nums,i,j);
+            }
+        }
+
+        Swap.swap(nums,i+1,high);
+        return i+1;
     }
 
 
     public static void main(String[] args) {
         int[] numbers = {12, 3, 3213, -42, 42, 1, 487};
         QuickSort quickSort = new QuickSort();
-        quickSort.quickSort(numbers);
+        quickSort.quickSort(numbers,0,numbers.length-1);
         Arrays.stream(numbers).forEach(num -> System.out.print(num + " "));
     }
 }
