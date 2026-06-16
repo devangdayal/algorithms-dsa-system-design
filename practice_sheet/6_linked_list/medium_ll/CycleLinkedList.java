4698b7a4db88f749aab077172e5ba018d50ea953
Devang Dayal <devangdayal@gmail.com>
2026-06-16 18:40:19 +0530

    Cycle in LL

---
+++ practice_sheet/6_linked_list/medium_ll/CycleLinkedList.java
@@ -0,0 +1,19 @@
+package medium_ll;
+
+public class CycleLinkedList {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
