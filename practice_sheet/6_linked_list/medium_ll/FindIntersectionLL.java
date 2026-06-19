fbbd9b97761e775a8839cef699d15511d839fb09
Devang Dayal <devangdayal@gmail.com>
2026-06-19 13:34:16 +0530

    FindIntersectionLL

---
+++ practice_sheet/6_linked_list/medium_ll/FindIntersectionLL.java
@@ -0,0 +1,15 @@
+package medium_ll;
+
+public class FindIntersectionLL {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while (a != b) {
+            a = a != null ? a.next : headB;
+            b = b != null ? b.next : headA;
+        }
+
+        return a;
+    }
+}
