a84814c4382bfd79fd35cb0831bb18ff453b0841
Devang Dayal <devangdayal@gmail.com>
2026-06-19 13:49:53 +0530

    Add Two Numbers

---
+++ practice_sheet/6_linked_list/medium_ll/AddTwoNumberLL.java
@@ -0,0 +1,30 @@
+package medium_ll;
+
+public class AddTwoNumberLL {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode result = new ListNode(0);
+        ListNode curr = result;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int value = x + y + carry;
+            carry = value / 10;
+            value %= 10;
+
+            curr.next = new ListNode(value);
+            curr = curr.next;
+            if (l1 != null)
+                l1 = l1.next;
+
+            if (l2 != null)
+                l2 = l2.next;
+
+        }
+
+        return result.next;
+    }
+}
